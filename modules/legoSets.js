a922c7f7de666fcae26afc7c36fcf534aa39b11d
martin3836 <139516862+martin3836@users.noreply.github.com>
2024-07-18 15:58:31 -0400

    Update legoSets.js

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,62 +1,80 @@
-const setData = require("../data/setData");
-const themeData = require("../data/themeData");
+const setData = require("../data/setData.json");
+const themeData = require("../data/themeData.json");
 
 let sets = [];
 
-const initialize = () => {
-  return new Promise((resolve, reject) => {
-    try {
-      setData.forEach((element) => {
-        sets.push(element);
-      });
+//stucture
+// themeData = {
+//     "id": "686",
+//     "name": "Chinese Traditional Festivals"
+// },
+// setData =  {
+//     "set_num": "0003977811-1",
+//     "name": "Ninjago: Book of Adventures",
+//     "year": "2022",
+//     "theme_id": "761",
+//     "num_parts": "1",
+//     "img_url": "https://cdn.rebrickable.com/media/sets/0003977811-1.jpg"
+// },
 
-      sets.forEach((set) => {
-        set.theme = themeData.find((theme) => theme.id === set.theme_id).name;
-      });
-
-      resolve(console.log("The 'sets' array has been initialized"));
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
-
-const getAllSets = () => {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(sets);
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
-
-const getSetByNum = (setNum) => {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(sets.find((set) => set.set_num === setNum));
-    } catch (error) {
-      reject(error, "Unable to find set");
-    }
-  });
-};
-
-const getSetsByTheme = (theme) => {
-  //must format so each word is lowercase
-  return new Promise((resolve, reject) => {
-    try {
-      const filterredSets = sets.filter((set) =>
-        set.theme.toLowerCase().includes(theme.toLowerCase())
-      );
-      if (filterredSets.length === 0) {
-        throw "Unable to find sets by theme";
+async function initialize() {
+  try {
+    setData.forEach((set) => {
+      const theme = themeData.find((theme) => theme.id === set.theme_id);
+      const updatedSet = { ...set };
+      if (theme) {
+        updatedSet.theme = theme.name;
       } else {
-        resolve(filterredSets);
+        updatedSet.theme = "unknown";
       }
-    } catch (error) {
-      reject(error, "Unable to find sets by theme");
+
+      sets.push(updatedSet);
+    });
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getAllSets() {
+  try {
+    return sets;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getSetByNum(setNum) {
+  try {
+    const foundSet = sets.find((set) => set.set_num === setNum);
+    if (foundSet) {
+      return foundSet;
+    } else {
+      throw new Error("unable to find requested set");
     }
-  });
-};
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getSetsByTheme(theme) {
+  try {
+    const newSet = sets.filter((set) =>
+      set.theme.toLowerCase().includes(theme.toLowerCase())
+    );
+    if (newSet.length > 0) {
+      return newSet;
+    } else {
+      throw new Error("unable to find requested sets");
+    }
+  } catch (err) {
+    throw err;
+  }
+}
 
 module.exports = { initialize, getAllSets, getSetByNum, getSetsByTheme };
+//console.log(getAllSets());
+//console.log(getSetByNum("001-1"));
+
+//console.log(getSetsByTheme("TECH"))
+
+//console.log(sets.length)
